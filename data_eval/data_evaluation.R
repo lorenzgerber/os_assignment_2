cbeb79c06b906eeedff5e3c2da23fe8beb6fd89a
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-10-08 13:08:35 +0200

    working on data eval

--- data_eval/data_evaluation.R
+++ data_eval/data_evaluation.R
@@ -10,7 +10,7 @@ colnames(benchmarking)<-header
 attach(benchmarking)
 
 
-other_inc <- filter(benchmarking, scheduler == "sched_other", load_type == "inc")
+other_inc <- filter(benchmarking, scheduler == "sched_other", load_type == "incr")
 by_rank<-group_by(other_inc, thread_rank)
 summarize(by_rank, sd(cpu_time), sd(wall_time), sd(wall_create_to_run), sd(run_to_finish))
 summarize(by_rank, mean(cpu_time), mean(wall_time), mean(wall_create_to_run), mean(run_to_finish))
@@ -19,7 +19,7 @@ summarize(by_rank, 'RSD cpu time' = 100/mean(cpu_time)*sd(cpu_time),
           'RSD create to run' = 100/mean(wall_create_to_run)*sd(wall_create_to_run), 
           'RSD run to finish' = 100/mean(run_to_finish)*sd(run_to_finish))
 
-rr_inc <- filter(benchmarking, scheduler == "sched_rr", load_type == "inc")
+rr_inc <- filter(benchmarking, scheduler == "sched_rr", load_type == "incr")
 by_rank<-group_by(rr_inc, thread_rank)
 summarize(by_rank, sd(cpu_time), sd(wall_time), sd(wall_create_to_run), sd(run_to_finish))
 summarize(by_rank, mean(cpu_time), mean(wall_time), mean(wall_create_to_run), mean(run_to_finish))
